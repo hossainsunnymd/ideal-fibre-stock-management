73e5da8ce19b66c93eee6ebc69b8b40711893220
sunnymuhammad <riddoamhed@gmail.com>
2025-05-12 03:30:22 +0600

    changes in login page

--- resources/js/Pages/Auth/LoginPage.vue
+++ resources/js/Pages/Auth/LoginPage.vue
@@ -27,7 +27,7 @@ function submitForm() {
 <!-- component -->
 <div class="min-h-screen bg-gray-100 flex items-center justify-center p-4">
   <div class="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
-    <div class="d-flex justify-content-center mb-3">
+    <div class="flex items-center justify-center">
       <img class="h-[100px]" src="../../Assets/img/logo.jpg" alt="Logo"/>
     </div>
 
